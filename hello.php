519e648f51fbf835bc4ce4fd4d8005aff5da3bdb
jerryleblanc56 <jerryleblanc56@yahoo.com>
2014-09-20 17:48:20 -0700

    Create hello.php

---
+++ hello.php
@@ -0,0 +1,16 @@
+<?php print( '<?xml version = "1.0" encoding = "utf-8"?>' ) ?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+   "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+
+<html xmlns = "http://www.w3.org/1999/xhtml">
+<head>
+        <title>Hello PHP</title>
+        <h3>Hello PHP</h3>
+</head>
+<body>
+   <?php
+        echo "<p>hello PHP world <br />";
+        # phpinfo();
+   ?> <!-- end PHP -->
+</body>
+</html>
